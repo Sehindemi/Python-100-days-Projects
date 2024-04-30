cccff7d101e8aaec38aa1f1186f949fc7dcebc0a
Sehindemi Ajayi <Sehindemitech@gmail.com>
2024-04-30 19:41:42 +0100

    added decrypt section to the code

--- Day-8/Caesar-CIpher.py
+++ Day-8/Caesar-CIpher.py
@@ -1,18 +1,25 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-def encrypt(plain_text,shift_ammount):
-    cipher_text = ""
-
-    for letter in plain_text:
+def encrypt(plain_text, shift_amount):
+  cipher_text = ""
+  for letter in plain_text:
+    position = alphabet.index(letter)
+    new_position = position + shift_amount
+    cipher_text += alphabet[new_position]
+  print(f"The encoded text is {cipher_text}")
+def decrypt(cipher_text, shift_ammount):
+    decrypted_text=""
+    for letter in cipher_text:
         position = alphabet.index(letter)
-        new_position = position + shift_ammount
-        new_letter = alphabet[new_position]
-        cipher_text += new_letter
-    print(f"The cipher text is {cipher_text}")
+        new_position = position - shift_ammount
+        decrypted_text += alphabet[new_position]
+    print(f"The decoded text is {decrypted_text}")
 
-
-encrypt(plain_text=text, shift_ammount=shift)
+if direction == "encode":
+    encrypt(plain_text=text, shift_amount=shift)
+elif direction == "decode":
+    decrypt(cipher_text=text,shift_ammount=shift)
